d42562be63c4442b809508d666cb7afc9d78b692
danimtb <danimanzaneque@gmail.com>
2018-02-15 18:00:55 +0100

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, tools
+import os
+
+
+class lapackConan(ConanFile):
+    name = "relapack"
+    version = "1.0"
+    url = "https://github.com/HPAC/ReLAPACK"
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"shared": [True, False]}
+    default_options = "shared=True"
+    requires = "lapack/3.7.1@danimtb/testing"
+
+    def source(self):
+        source_url = ("%s/archive/v%s.zip" % (self.url, self.version))
+        tools.get(source_url)
+        os.rename("%s-%s" % (self.name, self.version), "sources")
+
+    def build(self):
+        self.run("cd sources && make")
+
+    def package(self):
+        self.copy(pattern="LICENSE", dst="licenses", src="sources", ignore_case=True, keep_path=False)
+        self.copy(pattern="*.h", dst="include", src="sources/inc", keep_path=False)
+        self.copy(pattern="*relapack*.dll", dst="bin", src="bin", keep_path=False)
+        self.copy(pattern="*relapack*.so*", dst="lib", src="lib", keep_path=False)
+        self.copy(pattern="*relapack*.dylib", dst="lib", src="lib", keep_path=False)
+        self.copy(pattern="*relapack*.lib", dst="lib", src="lib", keep_path=False)
+        self.copy(pattern="*relapack*.a", dst="lib", src=".", keep_path=False)
+        self.copy(pattern="*relapack*.a", dst="lib", src="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["relapack"]
