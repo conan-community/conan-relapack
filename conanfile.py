5a2ca86db9afb42d69476d99f568c902e7842c90
danimtb <danimanzaneque@gmail.com>
2018-06-27 12:12:08 +0200

    enchancements

--- conanfile.py
+++ conanfile.py
@@ -1,29 +1,40 @@
-from conans import ConanFile, tools, AutoToolsBuildEnvironment
 import os
+from conans import ConanFile, tools, AutoToolsBuildEnvironment
 
 
-class lapackConan(ConanFile):
+class relapackConan(ConanFile):
     name = "relapack"
     version = "1.0"
     url = "https://github.com/HPAC/ReLAPACK"
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False]}
     default_options = "shared=True"
-    requires = "lapack/3.7.1@danimtb/testing"
+    requires = "lapack/3.7.1@conan/stable"
+
+    @property
+    def source_subfolder(self):
+        return "sources"
+
+    def config_options(self):
+        if self.settings.compiler == "Visual Studio":
+            self.options["lapack"].visual_studio = True
 
     def source(self):
         source_url = ("%s/archive/v%s.zip" % (self.url, self.version))
         tools.get(source_url)
-        os.rename("%s-%s" % (self.name, self.version), "sources")
+        os.rename("%s-%s" % (self.name, self.version), self.source_subfolder)
 
     def build(self):
-        with tools.chdir("sources"):
-            env_build = AutoToolsBuildEnvironment(self)
-            env_build.make()
+        with tools.chdir(self.source_subfolder):
+            autotools = AutoToolsBuildEnvironment(self)
+            make = tools.get_env("CONAN_MAKE_PROGRAM", "make")
+            if tools.which("mingw32-make"):
+                make = "mingw32-make"
+            autotools.make(make_program=make)
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src="sources", ignore_case=True, keep_path=False)
-        self.copy(pattern="*.h", dst="include", src="sources/inc", keep_path=False)
+        self.copy(pattern="LICENSE", dst="licenses", src=self.source_subfolder, keep_path=False)
+        self.copy(pattern="*.h", dst="include", src="%s/inc" % self.source_subfolder, keep_path=False)
         self.copy(pattern="*relapack*.dll", dst="bin", src="bin", keep_path=False)
         self.copy(pattern="*relapack*.so*", dst="lib", src="lib", keep_path=False)
         self.copy(pattern="*relapack*.dylib", dst="lib", src="lib", keep_path=False)
@@ -32,4 +43,4 @@ class lapackConan(ConanFile):
         self.copy(pattern="*relapack*.a", dst="lib", src="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["relapack"]
+        self.cpp_info.libs = tools.collect_libs(self)
